0b99a2f5ae73f4a83609d2fd465c7dac20823bf6
saltshaker <phsalter@calpoly.edu>
2021-10-22 19:58:19 -0600

    The ramp detector may need some finagling to get right, but its working now.

--- LoadCalculations.m
+++ LoadCalculations.m
@@ -1,9 +1,5 @@
 % Peak, average load, and ramps
-% Variable set-up
-if exist("sortedData", "var") == 0
-    [time, data, sortedData, totalTime, totalData, totalSortedData] = CreateLoadArrays();
-end
-
+function [peaks, avgLoad, ramps] = LoadCalculations(time, data)
 %% Peak Load
 % Seems like there is never more than 5 prominent peaks in load
 % Entries with Power = 0 should be ignored when using the data
@@ -21,34 +17,25 @@ for i = 1:365
     end
 end
 
-% Plots load over a day with major peaks marked
-% PlotLoadPeaksDay(10, 10, time, data, peaks);
-
 %% Average Load
-averageLoad = zeros(365,1);
+avgLoad = zeros(365,1);
 
 for i = 1:365
-    averageLoad(i) = mean(data(i,:));
+    avgLoad(i) = mean(data(i,:));
 end
-
-% Plots
-% figure(11)
-% plot(averageLoad)
-% xlabel("Day")
-% ylabel("Average Load [MW]")
 
 %% High Ramp Periods
 % Entries with DeltaPower = 0 should be ignored when using the data
-peaks = repmat(struct("Time", datetime, "DeltaPower", 0), 365, 5);
-prominenceLimit = 0.5;
+ramps = repmat(struct("Time", datetime, "DeltaPower", 0), 365, 5);
+prominenceLimit = 0.6;
 
 for i = 1:365
     [~, P] = islocalmax(diff(data(i,:)));
     timeBuf = time(i, :);
     dataBuf = data(i,:);
-    timePeaks = timeBuf(P > prominenceLimit);
-    dataPeaks = dataBuf(P > prominenceLimit);
-    for j = 1:length(timePeaks)
-        peaks(i, j) = struct("Time", timePeaks(j), "Power", dataPeaks(j));
+    timeRampPeaks = timeBuf(P > prominenceLimit);
+    dataRampPeaks = dataBuf(P > prominenceLimit);
+    for j = 1:length(timeRampPeaks)
+        ramps(i, j) = struct("Time", timeRampPeaks(j), "DeltaPower", dataRampPeaks(j));
     end
 end
--- PlotDayExample.m
+++ PlotDayExample.m
@@ -3,8 +3,15 @@
 if exist("sortedData", "var") == 0
     [time, data, sortedData, totalTime, totalData, totalSortedData] = CreateLoadArrays();
 end
-day = 272;
+day = 80;
 figNum = 10;
 
 PlotLoadDay(day, figNum, time, data);
 PlotLoadDurationDay(day, figNum+1, time, sortedData);
+
+%% Plot load curve with peaks and high ramps marked for a specific day
+%day = 250;
+figNum = 12;
+
+[peaks, avgLoad, ramps] = LoadCalculations(time, data);
+PlotLoadPnR(day, figNum, time, data, peaks, ramps);
